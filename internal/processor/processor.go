a601c63efa33d53e20d5195819080bbc593cd5e6
Daniil Naumov <naumovd127@gmail.com>
2026-05-17 14:05:18 +0300

    add cases (kill monster) processor and help funcs formatTime impossibleMove

--- internal/processor/processor.go
+++ internal/processor/processor.go
@@ -5,6 +5,7 @@ import (
 	"dungeon-challenge/internal/event"
 	"dungeon-challenge/internal/player"
 	"fmt"
+	"time"
 )
 
 type Processor struct {
@@ -29,6 +30,19 @@ func (p *Processor) getOrCreatePlayer(id int) *player.Player {
 	return newPlayer
 }
 
+func formatTime(t time.Time) string {
+	return t.Format("15:04:05")
+}
+
+func (p *Processor) impossibleMove(event event.Event) {
+	fmt.Printf(
+		"[%s] Player [%d] makes imposible move [%d]\n",
+		formatTime(event.Time),
+		event.PlayerID,
+		event.EventID,
+	)
+}
+
 func (p *Processor) Process(event event.Event) {
 	pl := p.getOrCreatePlayer(event.PlayerID)
 	if pl.Dead {
@@ -42,27 +56,61 @@ func (p *Processor) Process(event event.Event) {
 		pl.Disqualified = true
 		fmt.Printf(
 			"[%s] Player [%d] is disqualified\n",
-			event.Time.Format("15:04:05"),
+			formatTime(event.Time),
 			event.PlayerID,
 		)
 		return
 	}
 
 	switch event.EventID {
+	//registration
 	case 1:
+		if pl.Registered {
+			p.impossibleMove(event)
+			return
+		}
 		pl.Registered = true
 		fmt.Printf(
 			"[%s] Player [%d] registered\n",
-			event.Time.Format("15:04:05"),
+			formatTime(event.Time),
 			event.PlayerID,
 		)
+	//enter
 	case 2:
+		if pl.InDungeon {
+			p.impossibleMove(event)
+			return
+		}
 		pl.InDungeon = true
+		pl.CurrentFloor = 1
+		pl.FloorStartTime = event.Time
 		pl.EnterTime = event.Time
 		fmt.Printf(
 			"[%s] Player [%d] entered the dungeon\n",
-			event.Time.Format("15:04:05"),
+			formatTime(event.Time),
 			event.PlayerID,
 		)
+	//kill monster
+	case 3:
+		if !pl.InDungeon ||
+			pl.CompletedFloors[pl.CurrentFloor] ||
+			pl.BossEntered ||
+			pl.FloorKills[pl.CurrentFloor] >= p.config.Monsters {
+			p.impossibleMove(event)
+			return
+		}
+
+		pl.FloorKills[pl.CurrentFloor]++
+		fmt.Printf(
+			"[%s] Player [%d] killed the monster\n",
+			formatTime(event.Time),
+			event.PlayerID,
+		)
+		if pl.FloorKills[pl.CurrentFloor] == p.config.Monsters {
+			pl.CompletedFloors[pl.CurrentFloor] = true
+			duration := event.Time.Sub(pl.FloorStartTime)
+			pl.FloorDurations = append(pl.FloorDurations, duration)
+
+		}
 	}
 }
