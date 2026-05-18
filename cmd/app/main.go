9d62ed29a15e838e00da0e796c5915a2477857ac
Daniil Naumov <naumovd127@gmail.com>
2026-05-19 00:53:25 +0300

    add main.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,1 +1,46 @@
 package main
+
+import (
+	"bufio"
+	"dungeon-challenge/internal/config"
+	"dungeon-challenge/internal/event"
+	"dungeon-challenge/internal/processor"
+	"fmt"
+	"os"
+)
+
+func main() {
+	cfg, err := config.LoadConfig("config.json")
+	if err != nil {
+		panic(err)
+	}
+
+	eventsFile, err := os.Open("events")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer eventsFile.Close()
+
+	processor := processor.NewProcessor(cfg)
+
+	scanner := bufio.NewScanner(eventsFile)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		ev, err := event.ParseEvent(line)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		processor.Process(ev)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+		return
+	}
+	processor.PrintReport()
+
+}
